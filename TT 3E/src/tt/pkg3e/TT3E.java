293c9e4a9d7238b2aa334286b809619ac896433f
creadaxe <43036021+creadaxe@users.noreply.github.com>
2018-09-07 11:08:57 +0700

    Pungky Dwi K - TT 3E
    Tugas 1

---
+++ TT 3E/src/tt/pkg3e/TT3E.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tt.pkg3e;
+
+/**
+ *
+ * @author Kurniawan
+ */
+public class TT3E {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i,j,jumlah =5;
+            for(i=1;i<=jumlah;i++){
+                for(j=1;j<=i;j++){
+                System.out.print("X");}
+            System.out.println("");
+            }
+    }
+}
